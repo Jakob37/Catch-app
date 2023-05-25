4465957a7b2899a372b65c9d6b6f861f682e60bd
Jakob Willforss <jakob.willforss@hotmail.com>
2023-05-25 21:05:38 +0200

    Setup commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+function App(): JSX.Element {
+  return <View>
+    <Text>Hello world!</Text>
+    <TextInput style ={{borderColor: "gray", borderWidth: 1 }}></TextInput>
+    </View>
+}
+
+export default App;
+
+// https://chat.openai.com/
