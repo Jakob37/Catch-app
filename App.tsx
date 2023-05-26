874c05050f6c6c9a5b711384c781ae5fdf28dc1b
Jakob Willforss <jakob.willforss@hotmail.com>
2023-05-26 20:54:22 +0200

    Minimal variant running

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import {
-  Text,
-  TextInput,
-  View
-} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useState} from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
 
 function App(): JSX.Element {
-  return <View>
-    <Text>Hello world!</Text>
-    <TextInput style ={{borderColor: "gray", borderWidth: 1 }}></TextInput>
+  const [value, setValue] = useState('');
+
+  const handleChangeText = (text: string) => {
+    setValue(text);
+  };
+
+  const handleSave = async () => {
+    try {
+      await AsyncStorage.setItem('@MyApp:myKey', value);
+      console.log(`${value} stored`);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  return (
+    <View>
+      <Text>Hello world!</Text>
+      <Text>Stored value: {value}</Text>
+      <TextInput
+        onChangeText={handleChangeText}
+        style={{borderColor: 'gray', borderWidth: 1}}></TextInput>
+      <Button title="Save" onPress={handleSave}></Button>
     </View>
+  );
 }
 
 export default App;
-
-// https://chat.openai.com/
