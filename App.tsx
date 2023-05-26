d819fd49195744da89879f00f2eb0f0ecf5bdf63
Jakob Willforss <jakob.willforss@hotmail.com>
2023-05-26 21:42:57 +0200

    Add basic screen navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React, {useState} from 'react';
 import {Button, Text, TextInput, View} from 'react-native';
+import {enableScreens} from 'react-native-screens';
+enableScreens();
 
-function App(): JSX.Element {
+function InputScreen({navigation}) {
   const [value, setValue] = useState('');
   const [textArray, setTextArray] = useState<string[]>([]);
 
@@ -24,19 +28,43 @@ function App(): JSX.Element {
 
   return (
     <View>
-      <Text>Hello world!</Text>
+      <Text>Hello world!!</Text>
       <Text>Stored value: {value}</Text>
       <TextInput
         value={value}
         onChangeText={handleChangeText}
         style={{borderColor: 'gray', borderWidth: 1}}></TextInput>
       <Button title="Save" onPress={handleSave}></Button>
+      <Button
+        title="Go to content view"
+        onPress={() => navigation.navigate('Details')}></Button>
       <View>
         {textArray.map((text, index) => (
           <Text key={index}>{text}</Text>
         ))}
       </View>
     </View>
+  );
+}
+
+function ContentScreen({navigation}) {
+  return (
+    <View>
+      <Text>Content screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={InputScreen} />
+        <Stack.Screen name="Details" component={ContentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
