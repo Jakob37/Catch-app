6fa65aac3e12dd52e0a1c2f66454e273019cd1db
Jakob Willforss <jakob.willforss@hotmail.com>
2023-06-22 15:07:23 +0200

    Number padding

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,8 +1,10 @@
 function formatDate(rawDate: string): string {
   const parsedDate = new Date(rawDate);
+  const hours = String(parsedDate.getHours()).padStart(2, '0');
+  const minutes = String(parsedDate.getMinutes()).padStart(2, '0');
   return `${String(
     parsedDate.getFullYear(),
-  )}/${parsedDate.getMonth()}/${parsedDate.getDate()} ${parsedDate.getHours()}:${parsedDate.getMinutes()}`;
+  )}/${parsedDate.getMonth()}/${parsedDate.getDate()} ${hours}:${minutes}`;
 }
 
 export {formatDate};
